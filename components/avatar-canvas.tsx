a7f9d83b54f28f1f92286fb79da2399169455e5e
shuhei tanaka <shuhei46491@gmail.com>
2025-03-12 10:14:23 +0900

    feat: avatar-canvas

---
+++ components/avatar-canvas.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React, { useRef } from "react";
+import { Canvas } from "@react-three/fiber";
+import Model from "@/components/model";
+import { OrbitControls } from "@react-three/drei";
+
+const model = {
+  model: "/models/avatar.vrm",
+  title: "Test Model",
+  desctiption:
+    "This is a test model to demonstrate the use of VRM models in a Next.js app.",
+};
+
+export default function AvatarCanvas() {
+  const gltfCanvasParentRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <div ref={gltfCanvasParentRef} style={{ height: 700, width: 600 }}>
+      <Canvas
+        frameloop="demand"
+        camera={{ fov: 20, near: 0.1, far: 300, position: [0, 1, -7] }}
+        flat
+      >
+        <mesh position={[0, -1, 0]}>
+          <directionalLight position={[1, 1, -1]} color={"0xFFFFFF"} />
+          <Model url={model.model} />
+          <color attach="background" args={["#f7f7f7"]} />
+          <OrbitControls
+            enableZoom={true}
+            enablePan={false}
+            enableDamping={false}
+          />
+          <gridHelper />
+        </mesh>
+      </Canvas>
+    </div>
+  );
+}
